c2d7daf06e2d6a32d76a38ca9ae1f809c9af90a3
Ahmed-Almakki <138536735+Ahmed-Almakki@users.noreply.github.com>
2026-03-22 21:00:17 +0000

    first part of the pipline untile the hyperopt

---
+++ 03-orchestration/prefect_pipeline.py
@@ -0,0 +1,98 @@
+"""
+Pipeline Model Deployment
+"""
+from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
+from hyperopt.pyll import scope
+import mlflow
+import numpy as np
+import pandas as pd
+from prefect import flow, task
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+from sklearn.model_selection import train_test_split, cross_val_score
+
+mlflow.set_tracking_uri("http://127.0.0.1")
+mlflow.set_experiment("RandomForestPrefect")
+
+
+
+def load_data(path: str) -> pd.DataFrame:
+    """
+    Load data using pandas parquet and just return the first 1000 record becuase of memeory
+    """
+    df = pd.read_parquet(path).head(1000)
+    return df
+
+
+def process_data(df: pd.DataFrame) -> tuple:
+    """
+    Clean and exctract usfull data for the model
+    """
+    df['PU_DO'] = df['PULocationID'].astype(str) + '_' + df['DOLocationID'].astype(str)
+    df_encoded = pd.get_dummies(df[['PU_DO', 'trip_distance']], columns=['PU_DO'])
+    df_encoded["duration"] = df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']
+    df_encoded.duration = df_encoded.duration.apply(lambda td: td.total_seconds() / 60)
+    Y = df_encoded.pop("duration")
+    return df_encoded, Y
+
+
+
+def firstTrain(X_train: pd.DataFrame, Y_train: np.ndarray, X_val: pd.DataFrame, Y_val: np.ndarray):
+    """
+    Start the first stage of training to exctract the best parameter
+    """
+
+    def hyperTrain(params: dict) -> dict:
+        """
+        Log and Train using hyperopt so you can choose the best models
+        """
+        with mlflow.start_run(nested=True):
+            mlflow.set_tag("model", "RandomForset")
+            mlflow.sklearn.autolog()
+
+            model = RandomForestRegressor(**params, random_state=42)
+            model.fit(X_train, Y_train)
+
+            # Perform 5-Fold Cross Validation. 
+            # n_jobs=-1 tells it to use all your CPU cores to speed it up.
+            # Scikit-learn uses negative values for errors, so we ask for 'neg_root_mean_squared_error'
+            score = cross_val_score(
+                model, X_train, Y_train, cv=5, scoring="neg_root_mean_squared_error", n_jobs=-1
+            )
+            rmse = abs(score.mean())
+            
+            # y_pred = model.predict(X_val)
+            # rmse = root_mean_squared_error(y_pred=y_pred, y_true=Y_val)
+
+            mlflow.log_metrics({"rmse": rmse})
+
+        return {"loss": rmse, "status": STATUS_OK}
+    
+
+    space = {
+        "n_estimators": scope.int(hp.quniform("n_estimators", 100, 200, 1)),
+        "max_depth": scope.int(hp.quniform("max_depth", 10, 40, 1)),
+        "min_samples_split": scope.int(hp.quniform("min_samples_split", 2, 5, 1)),
+        "min_weight_fraction_leaf": hp.uniform("min_weight_fraction_leaf", 0.0, 0.5),
+        "bootstrap": hp.choice("bootstrap", [True, False]),
+    }
+
+    rstate = np.random.default_rng(42)
+    trails = Trials()
+
+    with mlflow.start_run(run_name="HyperOpt_Optmization"):
+        best_params = fmin(
+            fn=hyperTrain,
+            space=space,
+            algo=tpe.suggest,
+            max_evals=15,
+            trials=trails,
+            rstate=rstate,
+        )
+
+    return best_params
+
+
+
+    
+
